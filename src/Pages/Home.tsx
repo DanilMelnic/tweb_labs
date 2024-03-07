c8764e0ed66dea4256a86f7d06ad4f790a395640
danil <mielnik.danil2003@gmail.com>
2024-03-07 11:38:22 +0300

    аше сообщение о коммите

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
+import zalImage from '../img/zal1.jpg'; // Путь к изображению тренажерного зала
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+const Home = () => {
+  return (
+    <div>
+      <h2>Добро пожаловать в наш тренажерный зал!</h2>
+      <img src={zalImage} alt="Тренажерный зал" style={{ width: '100%', maxWidth: '800px', height: 'auto', display: 'block', margin: '0 auto' }} />
+      <p>Мы рады приветствовать вас на нашем сайте. Здесь вы найдете все необходимое для занятий спортом и достижения своих целей в фитнесе!</p>
+      <p>Планируйте тренировки, следите за своим прогрессом, участвуйте в групповых занятиях и многое другое.</p>
+    </div>
+  );
 }
 
 export default Home;
